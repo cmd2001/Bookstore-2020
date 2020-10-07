8a045e3f5c9ea6c0c6028bcef9bb6796986d5b6c
Amagi_Yukisaki <xingzeyu2001@gmail.com>
2020-10-07 12:05:26 +0800

    core complete

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,6 @@
-#include <iostream>
-#include <cstdio>
-#include <cstring>
-#include <cassert>
-#include <algorithm>
-#include "database_cached.hpp"
-#define debug cerr
-using namespace std;
-using __Amagi::database_cached; // B+ Tree with rb-tree cache.
+#include "everything.hpp"
 
 int main() {
-    debug << "Build ME" << endl;
+    debug << "Build Complete!" << endl;
     return 0;
 }
